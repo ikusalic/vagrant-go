16eef35995dff1e0f1a6b37c83a68bbd14c1ae1f
Ivan Kusalic <ivan@ikusalic.com>
2014-03-01 16:50:43 +0100

    Update Vim recipe

--- vagrant-go-wrapper-cookbook/recipes/default.rb
+++ vagrant-go-wrapper-cookbook/recipes/default.rb
@@ -12,10 +12,9 @@ include_recipe 'rvm::gem_package'
 
 node.override['vim_setup']['custom_preinstall_bash'] = <<-HERE  # build custom vim
   yum groupinstall -y 'Development Tools'
-  yum install -y mercurial
   yum install -y perl-devel python-devel perl-ExtUtils-Embed ncurses-devel
 HERE
-node.override['vim_setup']['custom_bash_user'] = <<-HERE
+node.override['vim_setup']['custom_bash_user_before_vundle'] = <<-HERE
   # clone manaully so the Vundle's BundleInstall command works from shell
   git clone https://github.com/Lokaltog/vim-distinguished.git ~/.vim/bundle/vim-distinguished
 HERE
@@ -47,7 +46,7 @@ HERE
 node.override['vim_setup']['build_from_source'] = true
 node.override['vim_setup']['vundle_timeout'] = 1000
 node.override['vim_setup']['dotfiles_repo'] = 'https://github.com/ikusalic/dotfiles.git'
-node.override['vim_setup']['global'] = false
+node.override['vim_setup']['global_vimrc'] = false
 node.override['vim_setup']['users'] = [ 'vagrant' ]
 node.override['vim_setup']['use_vundle'] = true
 include_recipe 'vim-setup'
