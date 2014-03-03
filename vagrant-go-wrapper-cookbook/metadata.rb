cd77698c5cdc6ee6814af7e9630de0dcf93aff5f
Ivan Kusalic <ivan@ikusalic.com>
2014-03-03 15:36:44 +0100

    Update wrapper cookbook metadata

--- vagrant-go-wrapper-cookbook/metadata.rb
+++ vagrant-go-wrapper-cookbook/metadata.rb
@@ -3,7 +3,9 @@ maintainer_email 'ivan@ikusalic.com'
 description      'Wrapper cookbook for vagrant-go'
 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.2'
+
+supports 'centos'
 
 depends 'yum'
 depends 'git'
